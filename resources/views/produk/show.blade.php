27815fa0552d509a57d6e7e1063a8fd3033b9b6f
nizayulia <nizayulia@gmail.com>
2020-10-30 09:01:08 +0700

    add modal cart

--- resources/views/produk/show.blade.php
+++ resources/views/produk/show.blade.php
@@ -37,11 +37,92 @@
                                 </dl>  <!-- item-property .// -->
                             </div> <!-- col.// -->
                         </div> <!-- row.// -->
-                        <p><a class="btn btn-primary" href=""><i class="fa fa-shopping-cart"></i> Beli Sekarang</a></p>
+                        <p><a class="btn btn-primary" data-toggle="modal" data-target="#cartModal" href=""><i class="fa fa-shopping-cart"></i> Beli Sekarang</a></p>
                 </article> <!-- card-body.// -->
         </div> <!-- row.// -->
     </div> <!-- card.// -->
 </div>
 <!--container.//-->
+
+
+<!-- modal add to cart -->
+<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header border-bottom-0">
+        <h5 class="modal-title" id="exampleModalLabel">
+          Keranjang Belanja
+        </h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <table class="table table-image">
+          <thead>
+            <tr>
+              <th scope="col"></th>
+              <th scope="col">Produk</th>
+              <th scope="col">Harga</th>
+              <th scope="col">Jumlah</th>
+              <th scope="col">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="w-25">
+                <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/vans.png" class="img-fluid img-thumbnail" alt="Sheep">
+              </td>
+              <td>Vans Sk8-Hi MTE Shoes</td>
+              <td>1700000</td>
+              <td class="qty"><input type="text" class="form-control" id="input1" value="2"></td>
+              <td>Rp. 1700000</td>
+            </tr>
+          </tbody>
+        </table> 
+        <div class="d-flex justify-content-end">
+          <h5>Total: <span class="price text-success">Rp. 1700000</span></h5>
+        </div>
+      </div>
+      <div class="modal-footer border-top-0 d-flex justify-content-between">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">Checkout</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- style modal add to cart -->
+<style>
+.container {
+  padding: 2rem 0rem;
+}
+
+.table-image {
+  
+  thead {
+    td, th {
+      border: 0;
+      color: #666;
+      font-size: 0.8rem;
+    }
+  }
+  
+  td, th {
+    vertical-align: middle;
+    text-align: center;
+    
+    &.qty {
+      max-width: 2rem;
+    }
+  }
+}
+
+.price {
+  margin-left: 1rem;
+}
+
+.modal-footer {
+  padding-top: 0rem;
+}</style>
 
 @endsection
